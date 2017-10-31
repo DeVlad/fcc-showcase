3605b3f605b101619c1b2dd87f58d7659e826267
Deyan Vladkov <deian.vladkov@gmail.com>
2017-10-31 20:37:46 +0100

    Filled some cards

--- src/js/fcc-showcase.js
+++ src/js/fcc-showcase.js
@@ -1,6 +1,10 @@
 // Let's code
 
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
+$(document).ready(function () {
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+    
+
+})
