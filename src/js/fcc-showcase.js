3a2a7e0215b3da31481bfcba1ecd73e23d370d24
Deyan Vladkov <deian.vladkov@gmail.com>
2017-11-02 13:03:11 +0100

    Added tech stack card icons

--- src/js/fcc-showcase.js
+++ src/js/fcc-showcase.js
@@ -1,10 +1,6 @@
-// Let's code
-
 $(document).ready(function () {
     // Initialize collapse button
     $(".button-collapse").sideNav();
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     //$('.collapsible').collapsible();
-    
-
 })
